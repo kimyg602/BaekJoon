434acbc711568429cc75e2260c7337e0b173bcef
kimyg602 <65595086+kimyg602@users.noreply.github.com>
2023-03-15 23:53:16 +0900

    Update BJ_25556.java
    수열을 오름차순으로 정렬하기 위해서는 큰 수부터 꺼내야 한다. = 스택에 넣을 때 나보다 작은 수 위로 쌓여야 한다
    빈 스택이면 그냥 추가 - 스택의 top값을 peek해서 넣으려는 원소값과 비교 후 작으면 추가, 아니면 다른 스택으로 이동해서 다시 비교
    해서 스택에 모든 원소를 추가하기만 하면 조건에 부합(YES) 아니면 (NO)

--- BJ_25556.java
+++ BJ_25556.java
@@ -3,34 +3,35 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-//      int[] arr = {4, 3, 6, 7, 8, 9, 10, 2, 1, 5};
-//      int[] arr = {5, 4, 3, 2, 1};
+//      int[] arr = {4, 3, 6, 7, 8, 9, 10, 2, 1, 5}; // 예시 case1
+//      int[] arr = {5, 4, 3, 2, 1}; // 예시 case2
+        
         Scanner sc = new Scanner(System.in);
 //        System.out.print("입력원소 갯수: ");
-        int length = sc.nextInt();
+        int length = sc.nextInt(); // 수열 사이즈 입력
         int[] arr = new int[length];
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) { // 수열의 원소 입력
 //        System.out.print("원소 입력: ");
             arr[i] = sc.nextInt();
         }
 
-        Stack<Integer>[] stack = new Stack[4];
+        Stack<Integer>[] stack = new Stack[4]; // 4스택 생성
         for (int i = 0; i < 4; i++) {
             stack[i] = new Stack<>();
         }
 
-        boolean yesOrNo = false;
+        boolean yesOrNo = false; // 초기화
         for (int i = 0; i < length; i++) {
-            yesOrNo = false;
+            yesOrNo = false; // 일다 매 스택에 삽입 완료 전까지 false(NO)
             for (int j = 0; j < 4; j++) {
                 if (stack[j].empty()){
                     stack[j].push(arr[i]);
-                    yesOrNo = true;
+                    yesOrNo = true; // 빈 스택에 삽입시 YES
                     break;
                 }
                 else if(stack[j].peek() < arr[i]){
                     stack[j].push(arr[i]);
-                    yesOrNo = true;
+                    yesOrNo = true; // top값이 넣으려는 원소보다 작을 경우 삽입성공 YES
                     break;
                 }
             }
